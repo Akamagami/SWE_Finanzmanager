6fdb1e35e695d11ae7f717c0690f8d28372150ab
akamagami <FelixRadermacher@gmx.de>
2021-11-05 18:26:41 +0100

    get JavaFX running

---
+++ src/backend/nutzer/Nutzer.java
@@ -0,0 +1,11 @@
+package backend.nutzer;
+
+public class Nutzer {
+
+	private Name name;
+	private int icon; //number will get changed translated to icon in UI
+	private int id; // assigned by element factory
+	
+	
+	
+}
