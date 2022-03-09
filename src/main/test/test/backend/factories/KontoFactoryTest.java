571687477e612127180216786123574816912dae
Akamagami <FelixRadermacher@gmx.de>
2022-03-09 14:25:12 +0100

    add junit

---
+++ src/main/test/test/backend/factories/KontoFactoryTest.java
@@ -0,0 +1,14 @@
+package test.backend.factories;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+import org.junit.jupiter.api.Test;
+
+public class KontoFactoryTest {
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
